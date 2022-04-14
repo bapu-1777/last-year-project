8c89806157fc8e5b9ce24d0c9237921a94489050
Maynk Vekariya <mayankpv2000@gmail.com>
2022-04-15 02:03:03 +0530

    first

---
+++ app/src/main/java/com/example/lastchance/SingupActivity.java
@@ -0,0 +1,16 @@
+package com.example.lastchance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+
+
+    }
+}
