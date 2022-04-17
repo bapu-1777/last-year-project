25b68fa0897751082b8cdf66b930201f7eb38120
Maynk Vekariya <mayankpv2000@gmail.com>
2022-04-17 12:07:12 +0530

    first commit

--- app/src/main/java/com/example/lastchance/LoginActivity.java
+++ app/src/main/java/com/example/lastchance/LoginActivity.java
@@ -2,13 +2,43 @@ package com.example.lastchance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText userNameEdt, passwordEdt;
+    private Button loginBtn;
+    private TextView newUserTV;
+    private FirebaseAuth mAuth;
+//    private ProgressBar loadingPB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userNameEdt = findViewById(R.id.idEdtUserName);
+        passwordEdt = findViewById(R.id.idEdtPassword);
+        loginBtn = findViewById(R.id.idBtnLogin);
+        newUserTV = findViewById(R.id.idTVNewUser);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        newUserTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(LoginActivity.this,SingupActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
